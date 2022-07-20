8129041d2028a8fe9c0e1d43747ad6d748b07103
antoniovilleda <villeda.gob.datos@gmail.com>
2022-07-20 02:30:49 -0500

    First Dag

---
+++ terraforms-templates/DAGs/ingestion_to_db.py
@@ -0,0 +1,13 @@
+from invoke import task
+from airflow.models import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+
+with DAG('db_ingestion', start_date = datetime.date(2022, 1, 1)) as dag:
+    start_workflow = DummyOperator(task_id = 'start_workflow')
+    validate = DummyOperator(task_id = 'validate')
+    prepare = DummyOperator(task_id = 'prepare')
+    load = DummyOperator(task_id = 'load')
+    end_workflow = DummyOperator(task_id = 'end_workflow')
+
+    start_workflow >> validate >> prepare >> load >> end_workflow

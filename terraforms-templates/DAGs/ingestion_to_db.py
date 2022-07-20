e827eac4dd2421857ad825abbe1ce8dc9307a089
antoniovilleda <villeda.gob.datos@gmail.com>
2022-07-20 03:38:48 -0500

    Postgres operator and table added

--- terraforms-templates/DAGs/ingestion_to_db.py
+++ terraforms-templates/DAGs/ingestion_to_db.py
@@ -1,11 +1,28 @@
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.dates import days_ago
 
-with DAG('db_ingestion', start_date = days_ago(1)) as dag:
+with DAG('db_ingestion', start_date = days_ago(1), schedule_interval = '@once') as dag:
     start_workflow = DummyOperator(task_id = 'start_workflow')
     validate = DummyOperator(task_id = 'validate')
-    prepare = DummyOperator(task_id = 'prepare')
+    prepare = PostgresOperator(
+        task_id = 'prepare',
+        postgres_conn_id = 'rds_connection',
+        sql = '''
+            CREATE SCHEMA purchase_raw;
+            CREATE TABLE purchase_raw.user_purchase (
+                invoice_number varchar(10),
+                stock_code varchar(20),
+                detail varchar(1000),
+                quantity int,
+                invoice_date timestamp,
+                unit_price numeric(8,3),
+                customer_id int,
+                country varchar(20)
+            );
+        '''
+    )
     load = DummyOperator(task_id = 'load')
     end_workflow = DummyOperator(task_id = 'end_workflow')
 

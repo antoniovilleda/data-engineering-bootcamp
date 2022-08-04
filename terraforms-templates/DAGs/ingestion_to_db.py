e40d14af3ff717cf89419559426f1f8682080652
antoniovilleda <villeda.gob.datos@gmail.com>
2022-08-04 00:57:00 -0500

    veamos

--- terraforms-templates/DAGs/ingestion_to_db.py
+++ terraforms-templates/DAGs/ingestion_to_db.py
@@ -9,7 +9,7 @@ from airflow.utils.trigger_rule import TriggerRule
 from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
-#   Function to ingest user_purchase table
+#   Function to ingest user_purchase table into postgres data base
 def ingest_data():
     s3_hook = S3Hook(aws_conn_id = 'aws_default')
     psql_hook = PostgresHook(postgres_conn_id = 'rds_connection')
